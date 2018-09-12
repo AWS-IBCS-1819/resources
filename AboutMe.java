68dbaf34917ab6b6d8dc52e112f8dbdb8305705c
Zack Considine <zack_considine@m013391.aw.org>
2018-09-12 12:52:37 -0700

    added scanner code

--- AboutMe.java
+++ AboutMe.java
@@ -15,10 +15,30 @@ public class AboutMe{
     age = 35;
   }
 
+  public void changeName(String newName) {
+    name = newName;
+  }
+
   public static void main(String[] args) {
 
     AboutMe mrC = new AboutMe(); // call constructor method
-    System.out.println("Hello my name is " + mrC.name);
+
+    System.out.println("Welcome to a bit about ME."
+    + " What would you like to know?:\n(name)\n(hair color)\n(age)\n"
+    + "(change name)");
+    Scanner newIn = new Scanner(System.in);
+
+    String userIn = newIn.next();
+    if (userIn.equals("name")){
+      System.out.println("Hello my name is " + mrC.name);
+    }
+    if (userIn.equals("change")){
+      // add prompt to select the name change
+      mrC.changeName("Ezekiel");
+      System.out.println("Hello my name is " + mrC.name);
+    }
+
+    newIn.close();
 
   }
 
