5f7857ad9e25d66ecf642df98e0801c74e14d89a
Zack Considine <zack_considine@m013391.aw.org>
2018-09-21 11:00:23 -0700

    added some helpful code

---
+++ gradebookTips.java
@@ -0,0 +1,16 @@
+//Useful code for Gradebook.java
+
+// method for converting string to double
+Double.parseDouble("the number you want to convert");
+
+// loop for going through a stringbuilder at every 5th interval
+// 1) i = 0
+// 2) i = 5
+// 3) i = 10
+// ...
+for (int i = 0; i < yourStringBuilder.length(); i = i + 5)
+
+// method for converting ints or doubles back to strings
+Integer.toString(yourNumber);
+
+Double.toString(yourNumber);
