6368997db7e48023f654af5d37f2f011aa36a089
Zack Considine <zack_considine@m013391.aw.org>
2018-09-21 13:28:24 -0700

    more added

--- gradebookTips.java
+++ gradebookTips.java
@@ -10,7 +10,19 @@ Double.parseDouble("the number you want to convert");
 // ...
 for (int i = 0; i < yourStringBuilder.length(); i = i + 5)
 
-// method for converting ints or doubles back to strings
-Integer.toString(yourNumber);
+// method for taking a substring of stringbuilder that is from index 4 - 7
+youStringBuilder.substring(4, 8));
 
+// method for converting integers back to strings
+Integer.toString(yourNumber);
+// method for converting doubles back to strings
 Double.toString(yourNumber);
+
+//try and catch for reading files
+try {
+  //read the file here
+}
+
+catch (FileNotFoundException e) {
+  e.printStackTrace();
+}
