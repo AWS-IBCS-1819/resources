3dc1687a89059b106d36d577f114a999d1bee75e
Zack Considine <zack_considine@m013391.aw.org>
2018-09-26 10:02:52 -0700

    updated tips

--- gradebookTips.java
+++ gradebookTips.java
@@ -1,5 +1,18 @@
 //Useful code for Gradebook.java
 
+//read next item as a string and append to StringBuilder
+yourStringBuilder.append(yourScanner.next());
+
+//String formatting
+"\n" // new Line
+"apples" +
+"bananas" // concatenate to be "applesbananas"
+
+// check to see user input = choice
+if (userInput.next().equals("1")) { // picked choice 1
+  // do stuff
+}
+
 // method for converting string to double
 Double.parseDouble("the number you want to convert");
 
@@ -11,16 +24,20 @@ Double.parseDouble("the number you want to convert");
 for (int i = 0; i < yourStringBuilder.length(); i = i + 5)
 
 // method for taking a substring of stringbuilder that is from index 4 - 7
-youStringBuilder.substring(4, 8));
+yourStringBuilder.substring(4, 8));
 
 // method for converting integers back to strings
 Integer.toString(yourNumber);
 // method for converting doubles back to strings
 Double.toString(yourNumber);
 
+// method for finding the number of characters in your StringBuilder
+youStringBuilder.length();
+
 //try and catch for reading files
 try {
   //read the file here
+  //add contents to string builders
 }
 
 catch (FileNotFoundException e) {
