6e0796cb2fadcc42667a8b06d59b6d4c1087b947
Zack Considine <zack_considine@m013391.aw.org>
2018-10-11 12:40:50 -0700

    updated Card

---
+++ cardGames/Card.java
@@ -0,0 +1,31 @@
+/* Card.java created by Mr. Considine
+This file holds all of the data and methods
+for a playing card to be used in Deck and Pokerhand
+*/
+
+public class Card {
+
+  Int value;
+  String name;
+
+  public void setName(String n) {
+    
+  }
+
+  public void setValue(Int v) {
+
+  }
+
+  public String getName() {
+
+  }
+
+  public Integer getValue() {
+
+  }
+
+  public Card() {
+
+  }
+
+}
