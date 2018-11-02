02499f5e6b5bd6485a8b9320513829abdcfae50d
Zack Considine <zack_considine@m013391.aw.org>
2018-11-02 10:55:20 -0700

    added mapDemo

---
+++ mapDemo.java
@@ -0,0 +1,21 @@
+/* mapDemo.java by Mr. Considine
+10/31/18
+*/
+
+import java.util.*;
+
+public class mapDemo {
+
+  public static void main(String[] args) {
+
+    Map m = new HashMap<String, String>();
+    m.put("The Oddyssey", "Homer");
+    m.put("A Night in the Lonesome October", "Roger Zelazny");
+    m.put("Goosebumps: Monster Blood", "R. L. Stine");
+    m.put("Bunnicula", "James Howe");
+
+    System.out.println(m);
+
+  }
+
+}
