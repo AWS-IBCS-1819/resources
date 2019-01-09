339580cc23fce6e758d0f53ff584ad089a38a63e
Zack Considine <zack_considine@m013391.aw.org>
2019-01-09 11:01:58 -0800

    files needed for recommender

---
+++ Recommender.java
@@ -0,0 +1,32 @@
+// Recommender.java
+// This program holds all of the methods and
+// variables for recommending a movie for a friend
+// to watch.
+// The two approaches here are content-based filtering
+// and collaborative filtering. They both read files
+// that have respective data in order to score the movies.
+// The program outputs a list of the top 3 recommendations
+// for that person.
+
+public class Recommender {
+
+  public void collaborative(String movieName) {
+    //read collab file
+    //print top 3 recommendations
+  }
+
+  public void contentBased(String movieName) {
+    //read contentBased file
+    //print top 3 recommendations
+  }
+
+  public static void main(String[] args) {
+    //print Welcome
+    //print list of movies
+    //ask which one user has seen
+    //scan for the input
+    //call recommendation methods
+
+    //if possible go again
+  }
+}
